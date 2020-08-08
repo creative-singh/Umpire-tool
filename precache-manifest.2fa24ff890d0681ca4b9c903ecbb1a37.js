8d978cba9e1305727f015bcd80338ee5e75ac5f3
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-08-08 13:16:52 +0530

    Updates

---
+++ precache-manifest.2fa24ff890d0681ca4b9c903ecbb1a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6ea867e1f1d2c58645cebbd632b203",
+    "url": "/My-First-React-App/index.html"
+  },
+  {
+    "revision": "d46f87f4d614238d8c3b",
+    "url": "/My-First-React-App/static/css/main.42a8c389.chunk.css"
+  },
+  {
+    "revision": "26b2369bb511ec37efab",
+    "url": "/My-First-React-App/static/js/2.c12fcc64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-First-React-App/static/js/2.c12fcc64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46f87f4d614238d8c3b",
+    "url": "/My-First-React-App/static/js/main.5ea93f22.chunk.js"
+  },
+  {
+    "revision": "0a6f6b717f4a1e438b9d",
+    "url": "/My-First-React-App/static/js/runtime-main.dcc2dc26.js"
+  }
+]);
